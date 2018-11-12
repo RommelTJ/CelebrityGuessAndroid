d0cbce669ecd5380c3dab009247d7798e55e8ed3
Rommel Rico <me@rommelrico.com>
2018-11-11 20:58:34 -0800

    Placeholder for onClick method

--- app/src/main/java/com/rommelrico/celebrityguessandroid/MainActivity.kt
+++ app/src/main/java/com/rommelrico/celebrityguessandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rommelrico.celebrityguessandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun celebChosen(view: View) {
+        // Placeholder.
+    }
+
 }
